4c36373431121239d3d48012a2b87e01767e5bd4
Sofía González <sofia_1.6@hotmail.com>
2024-04-03 01:21:55 -0300

    reactandts

---
+++ src/components/Greeter.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface GreeterProps {
+  person: string;
+}
+
+function Greeter({ person }: GreeterProps): JSX.Element {
+  return (
+    <h1>Hello, {person}!</h1>
+  )
+}
+
+export default Greeter;
